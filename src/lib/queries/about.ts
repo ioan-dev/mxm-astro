d83d34e624883785fe1ac04e1ad7278d5da60e1e
ioan-dev <82202528+tumanoviv@users.noreply.github.com>
2026-09-10 14:39:10 +0300

    upd about page

--- src/lib/queries/about.ts
+++ src/lib/queries/about.ts
@@ -1,27 +1,30 @@
+// src/lib/queries/about.ts
 import { client, readItems, readSingleton } from '../directus.js';
 
-export const aboutPage = await client.request(
-  readSingleton('page_about', {
-    fields: [
-      {
-        blocks: [
-          'collection',
-          {
-            item: {
-              block_aboutpage_team: [
-                {
-                  intro_text: ['layout', 'headline', 'editor_top', 'editor_left', 'editor_right'],
-                },
-                {
-                  about_team: ['name', 'photo'],
-                },
-              ],
-              block_text: ['layout', 'headline', 'editor_top', 'editor_left', 'editor_right'],
-              section_form: ['headline'],
+export async function getAboutPage() {
+  return await client.request(
+    readSingleton('page_about', {
+      fields: [
+        {
+          blocks: [
+            'collection',
+            {
+              item: {
+                block_aboutpage_team: [
+                  {
+                    intro_text: ['layout', 'headline', 'editor_top', 'editor_left', 'editor_right'],
+                  },
+                  {
+                    about_team: ['name', 'photo'],
+                  },
+                ],
+                block_text: ['layout', 'headline', 'editor_top', 'editor_left', 'editor_right'],
+                section_form: ['headline'],
+              },
             },
-          },
-        ],
-      },
-    ],
-  })
-);
+          ],
+        },
+      ],
+    })
+  );
+}
